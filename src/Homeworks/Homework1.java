26ff387c13e0c26915be71d063ac46801db5f374
Arman Hakhverdyan <arman.hakhverdyan@bk.ru>
2023-01-27 21:13:30 +0400

    Homework1

--- src/Homeworks/Homework1.java
+++ src/Homeworks/Homework1.java
@@ -2,84 +2,84 @@ package Homeworks;
 
 public class Homework1 {
 
-        public static void main(String[] args) {
-            byte a = 1;
-            byte b = 2;
+    public static void main(String[] args) {
+        byte a = 1;
+        byte b = 2;
 
-            short c = 3;
-            short d = 4;
+        short c = 3;
+        short d = 4;
 
-            int e = 5;
-            int f = 6;
+        int e = 5;
+        int f = 6;
 
-            long g = 7;
-            long h = 8;
+        long g = 7;
+        long h = 8;
 
-            byte min = -128;
-            byte max = -127;
+        byte min = -128;
+        byte max = -127;
 
-            short minShort =  -32768;
-            short maxShort =  32767;
+        short minShort = -32768;
+        short maxShort = 32767;
 
-            int minInt = -2147483648;
-            int maxInt =  2147483647;
+        int minInt = -2147483648;
+        int maxInt = 2147483647;
 
-            long minLong = -9223372036854775808L;
-            long maxLong =  9223372036854775807L;
+        long minLong = -9223372036854775808L;
+        long maxLong = 9223372036854775807L;
 
-            float i = 9.1f;
-            float j = 9.2f;
+        float i = 9.1f;
+        float j = 9.2f;
 
-            double k = 9.3;
-            double l = 9.4;
+        double k = 9.3;
+        double l = 9.4;
 
-            float maxFloat = 3.4028235E+38F;
-            float minFloat = 1.4E-45F;
+        float maxFloat = 3.4028235E+38F;
+        float minFloat = 1.4E-45F;
 
-            double maxDouble = 1.7976931348623157E+308;
-            double minDouble = 4.9E-324;
+        double maxDouble = 1.7976931348623157E+308;
+        double minDouble = 4.9E-324;
 
-            char n ='a';
-            char o ='b';
+        char n = 'a';
+        char o = 'b';
 
-            char s = 77;
-            char w = 109;
-            char t = 57;
+        char s = 77;
+        char w = 109;
+        char t = 57;
 
-            boolean p =true;
-            boolean q =false;
+        boolean p = true;
+        boolean q = false;
 
-            int year = 201;
-            System.out.print (year);
+        int year = 201;
+        System.out.print(year);
 
-            System.out.println(s+" "+w+" "+t);
-            System.out.println(a+" "+b+" "+c+" "+d+" "+e+" "+f+" "+g+" "+h+" "+n+" "+o+" "+i+" "
-                    +j+" "+k+" "+l+" "+p+" "+q+" "+max+" "+min+" "+minShort+" "+maxShort+" "+maxInt+" "+minInt+" "
-                    +minLong+" "+maxLong+" "+maxFloat+" "+minFloat+" "+maxDouble+" "+minDouble);
+        System.out.println(s + " " + w + " " + t);
+        System.out.println(a + " " + b + " " + c + " " + d + " " + e + " " + f + " " + g + " " + h + " " + n + " " + o + " " + i + " "
+                + j + " " + k + " " + l + " " + p + " " + q + " " + max + " " + min + " " + minShort + " " + maxShort + " " + maxInt + " " + minInt + " "
+                + minLong + " " + maxLong + " " + maxFloat + " " + minFloat + " " + maxDouble + " " + minDouble);
 
-            //automatically casting
+        //automatically casting
 
-            int aa = 7;
-            long bb = aa;
+        int aa = 7;
+        long bb = aa;
 
-            System.out.println(bb);
+        System.out.println(bb);
 
-            int myInt = 7;
-            double myDouble = myInt;
+        int myInt = 7;
+        double myDouble = myInt;
 
-            System.out.println(myDouble);
+        System.out.println(myDouble);
 
-            //manual casting
+        //manual casting
 
-            double dd = 9.78d;
-            int ii = (int) dd;
+        double dd = 9.78d;
+        int ii = (int) dd;
 
-            System.out.println(ii);
+        System.out.println(ii);
 
-            int ff = 9999;
-            byte gg = (byte) ff;
+        int ff = 9999;
+        byte gg = (byte) ff;
 
-            System.out.println(gg);
+        System.out.println(gg);
 
 
 //        long max = 3123456789l;
@@ -93,10 +93,10 @@ public class Homework1 {
 //        int m,n = 11;
 //        m = n;
 
-            float x = 12.345f;
-            float v = 3456.091f;
+        float x = 12.345f;
+        float v = 3456.091f;
 
-            double y = 34.567839023;
-            double z = 8923.1234857f;
-        }
+        double y = 34.567839023;
+        double z = 8923.1234857f;
     }
+}
